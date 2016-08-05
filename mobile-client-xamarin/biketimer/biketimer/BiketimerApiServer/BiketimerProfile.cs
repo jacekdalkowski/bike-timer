6bd19031d19b7a508cfd9e9f0506ab32026c24ed
Jacek Dalkowski <jacekdalkowski@gmail.com>
2016-08-05 23:47:56 +0200

    Fixes user account response parsing. Adds controls for simple fields in debug view of biketimer account.

--- mobile-client-xamarin/biketimer/biketimer/BiketimerApiServer/BiketimerProfile.cs
+++ mobile-client-xamarin/biketimer/biketimer/BiketimerApiServer/BiketimerProfile.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Biketimer
 {
 	public class BiketimerProfile
 	{
 		public string Id { get; set; }
+
 		public string Email { get; set; }
+
+		[JsonProperty(PropertyName = "bt_name")]
 		public string BtName { get; set; }
+
 		public IEnumerable<string> Roles { get; set; }
+
 		public IEnumerable<string> Friends { get; set; }
 
+		[JsonProperty(PropertyName = "fb_id")]
 		public string FbId { get; set; }
+
+		[JsonProperty(PropertyName = "fb_name")]
 		public string FbName { get; set; }
+
+		[JsonProperty(PropertyName = "fb_surname")]
 		public string FbSurname { get; set; }
 	}
 }
