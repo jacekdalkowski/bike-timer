3109edef19af4125a4026f2e91335290497c9c16
Jacek Dalkowski <jacekdalkowski@gmail.com>
2016-08-26 00:47:45 +0200

    Adds mock action sheet for users stats.

--- mobile-client-xamarin/biketimer/biketimer/Views/Stats/UserStatsPage.xaml.cs
+++ mobile-client-xamarin/biketimer/biketimer/Views/Stats/UserStatsPage.xaml.cs
@@ -9,6 +9,11 @@ namespace Biketimer.Views.Stats
 	{
 		public ListView ListView { get { return listView; } }
 
+		public Button FilterByDateButton { get { return filterByDateButton; } }
+		public Button FilterBySpotButton { get { return filterBySpotButton; } }
+		public Button FilterBySegmentButton { get { return filterBySegmentButton; } }
+		public Button SortButton { get { return sortButton; } }
+
 		public UserStatsPage()
 		{
 			Title = "Me";
@@ -41,12 +46,41 @@ namespace Biketimer.Views.Stats
 			//	}
 			//};
 
+			FilterByDateButton.Clicked += OnFilterByDateClicked;
+			FilterBySpotButton.Clicked += OnFilterBySpotClicked;
+			FilterBySegmentButton.Clicked += OnFilterBySegmentClicked;
+			SortButton.Clicked += OnSortClicked;
+
 			ListView.ItemsSource = new RunItem[]
 			{
 				new RunItem { Title = "AAA" },
 				new RunItem { Title = "BBB" },
 				new RunItem { Title = "CCC" }
 			};
+		}
+
+		async void OnFilterByDateClicked(object sender, EventArgs e)
+		{
+			string[] spots = new string[] { "Today", "Last 2 days", "Last week" };
+			string selectedSpot = await DisplayActionSheet("Period", "Cancel", null, spots);
+		}
+
+		async void OnFilterBySpotClicked(object sender, EventArgs e)
+		{
+			string[] spots = new string[] { "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak", "Kouty", "Spicak" };
+			string selectedSpot = await DisplayActionSheet("Spot", "Cancel", null, spots);
+		}
+
+		async void OnFilterBySegmentClicked(object sender, EventArgs e)
+		{
+			string[] segments = new string[] { "Meadow", "Forest" };
+			string selectedSegment = await DisplayActionSheet("Segment", "Cancel", null, segments);
+		}
+
+		async void OnSortClicked(object sender, EventArgs e)
+		{
+			string[] segments = new string[] { "Time", "Date - current first", "Date - oldest first" };
+			var action = await DisplayActionSheet("Sort by", "Cancel", null, segments);
 		}
 
 		public class RunItem
